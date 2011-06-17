c4aa7d9b90ca3e5e0a16a8313327544b2f7a77d7
unknown <c45y@.(none)>
2011-06-17 18:09:13 +1000

    Proof of concept

---
+++ Log-Cleaner.pl
@@ -0,0 +1,40 @@
+#! C:\xampp\perl\bin\perl.exe
+
+# Log censoring script.
+#     - Lwc password censoring
+#     - IP censoring (possibly use a salt?)
+#
+# Usage: perl Log-Cleaner.pl [logfile] 
+#
+# Stephen McGregor (c45y) 17/06/2011
+
+print "Content-type: text/html\n\n";
+$RESOURCE = 'C:\Users\c45y\Git\Log-Cleaner\logs.txt';
+open DATA, "$RESOURCE" or die "can't open $RESOURCE $!";
+$outfile = $RESOURCE;
+my @loglist = <DATA>;
+close (DATA);
+$outfile =~ /(.+)(\.)(.+)/;
+open (OUT, ">> $1clean$2$3") or die "can't open file $!";
+foreach $string (@loglist) {
+	if ($string =~ m/(\d+)\.(\d+)\.(\d+)\.(\d+)/) {
+		$string =~ s/(\d+)\.(\d+)\.(\d+)\.(\d+)/$1\.$2\.###\.###/;
+	}
+	if ($string =~ m/(lwc -c password)(.+)/) {
+		$string =~ s/(lwc -c password)(.+)/$1 ###/;
+	}
+	if ($string =~ m/(cpassword)(.+)/) {
+		$string =~ s/(cpassword)(.+)/$1 ###/;
+	}
+	if ($string =~ m/(lwc -u)(.+)/) {
+		$string =~ s/(lwc -u)(.+)/$1 ###/;
+	}
+	if ($string =~ m/(lwc -unlock)(.+)/) {
+		$string =~ s/(lwc -unlock)(.+)/$1 ###/; 
+	}
+	if ($string =~ m/(cunlock)(.+)/) {
+		$string =~ s/(cunlock)(.+)/$1 ###/;
+	}
+	print OUT "$string";
+}
+close (OUT);
---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+##Log-Cleaner##
+
+Generates a 'cleaned' Minecraft log file.
+*	LWC passwords are removed
+*	IP addresses have the trailing half hashed
+
+#####To Do
+> Change the '#' replacement of IP addresses to a one-way hash, so Ip's can still be compared.
